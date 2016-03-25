75599fd49c5caaa33309eb293588b447c6f66f4c
Dallin Christensen <dallin.christensen@gmail.com>
2016-03-24 20:29:22 -0600

    Add some installs for Linux

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Simple setup.sh for configuring my Linux dev environment
 
+
 # Setup git stuff
 sudo apt-get install git
 git config --global user.name "Dallin Christensen"
 git config --global user.email dallin.christensen@gmail.com
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+# Guake (dropdown terminal)
+sudo apt-get install guake
 
 
 # Setup oh-my-zsh
@@ -15,4 +20,50 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 export ZSH_THEME="afowler"
 
 
-sudo apt-get install guake
+# Pithos (Pandora client)
+sudo apt-get install pithos
+sudo add-apt-repository ppa:pithos/ppa
+sudo apt-get update
+
+
+# Darktable
+# sudo add-apt-repository ppa:pmjdebruijn/darktable-release
+# sudo apt-get update
+# sudo apt-get install debhelper dpkg-dev fakeroot
+# sudo apt-get build-dep darktable
+# tar zxvf darktable_$VERSION.orig.tar.gz
+# cd darktable-$VERSION
+# tar zxvf ../darktable_$VERSION.debian.tar.gz
+# dpkg-buildpackage -rfakeroot
+
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+
+# Sublime Text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+
+# VLC
+sudo apt-get install vlc
+sudo apt-get install mozilla-plugin-vlc
+
+
+# Play on Linux (and Wine)
+sudo apt-get install wine
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo apt-get update
+sudo apt-get install playonlinux
+
+
+# Pushbullet
+udo add-apt-repository ppa:atareao/atareao
+sudo apt-get update
+sudo apt-get install pushbullet-indicator
