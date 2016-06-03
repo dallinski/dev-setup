7f9c125c0705747bc16d37d4411bef635c56ca71
Dallin Christensen <dallin.christensen@gmail.com>
2016-06-03 08:23:07 -0600

    Add Java to Linux install script

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -68,3 +68,15 @@ sudo apt-get install playonlinux
 sudo add-apt-repository ppa:atareao/atareao
 sudo apt-get update
 sudo apt-get install pushbullet-indicator
+
+
+# Java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+
+# Android Studio
+
+
+# alacarte
