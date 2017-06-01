562a656862668aa0a5f6a1b268e62cfb3afcbd21
Dallin Christensen <dallin.christensen@gmail.com>
2017-06-01 15:03:51 -0600

    Colorize branch and show on same line as repo path

--- Linux/update_all_git_repos.sh
+++ Linux/update_all_git_repos.sh
@@ -9,7 +9,6 @@ echo "\n\e[1mPulling in latest changes for all repositories...\e[0m\n"
 # Find all git repositories and update it to the latest revision of whichever branch is currently checked out
 for i in $(find . -name ".git" | cut -c 3-); do
     echo "";
-    echo "\e[33m"$i"\e[0m";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
@@ -17,9 +16,15 @@ for i in $(find . -name ".git" | cut -c 3-); do
 
     if branch=$(git symbolic-ref --short -q HEAD)
     then
-      echo "\e[0;34mon branch " $branch "\e[0m"
+      if [ $branch = "develop" ]; then
+        echo "\e[33m"$i" \e[0;32m" $branch "\e[0m"
+      elif [ $branch = "master" ]; then
+        echo "\e[33m"$i" \e[0;34m" $branch "\e[0m"
+      else
+        echo "\e[33m"$i" \e[0;35m" $branch "\e[0m"
+      fi
     else
-      echo "\e[0;31mnot on any branch\e[0m"
+      echo "\e[33m"$i" \e[0;31mnot on any branch\e[0m"
     fi
 
     if [[ -z $(git status -s) ]]
